b71b1c4a9f30b8005352a8001f7690d5e29a3d4e
ASTUDILLO-Victor <astudillo_developer@outlook.com>
2026-04-13 22:59:45 -0500

    primer push con structura base de proyecto y configuracion de git

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión de la aplicación: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estructura del proyecto:");
+Console.WriteLine("Configuración de proyecto: .csproj");
+Console.WriteLine("Carpeta src/: Código fuente");
+Console.WriteLine("Metadatos Configurados");
+Console.WriteLine();
+Console.WriteLine("proximo paso:agregar argumentos CLI y configuracion de repositorio en github");
